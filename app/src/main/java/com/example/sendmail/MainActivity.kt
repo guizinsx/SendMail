50f7693e6de98fd626c7e580e1b505612f5b6025
guizinsx <guizin51@gmail.com>
2024-05-11 02:23:23 -0300

    sendMainIntent e chooserIntent

--- app/src/main/java/com/example/sendmail/MainActivity.kt
+++ app/src/main/java/com/example/sendmail/MainActivity.kt
@@ -1,5 +1,15 @@
 package com.example.sendmail
 
+import android.content.Intent
+import android.content.Intent.ACTION_CHOOSER
+import android.content.Intent.ACTION_SENDTO
+import android.content.Intent.EXTRA_BCC
+import android.content.Intent.EXTRA_CC
+import android.content.Intent.EXTRA_EMAIL
+import android.content.Intent.EXTRA_INTENT
+import android.content.Intent.EXTRA_SUBJECT
+import android.content.Intent.EXTRA_TEXT
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sendmail.databinding.ActivityMainBinding
@@ -12,5 +22,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        amb.cleanBt.setOnClickListener{
+            with (amb){
+                toEt.setText("")
+                ccEt.setText("")
+                bccEt.setText("")
+                subjectEt.setText("")
+                messageEt.setText("")
+            }
+
+        }
+
+        amb.sendBt.setOnClickListener {
+            val sendMailIntent = Intent(ACTION_SENDTO)
+
+            with (sendMailIntent) {
+                putExtra(EXTRA_EMAIL, arrayOf(amb.toEt.text.toString()))
+                putExtra(EXTRA_CC, arrayOf(amb.ccEt.text.toString()))
+                putExtra(EXTRA_BCC, arrayOf(amb.bccEt.text.toString()))
+                putExtra(EXTRA_SUBJECT, arrayOf(amb.subjectEt.text.toString()))
+                putExtra(EXTRA_TEXT, arrayOf(amb.messageEt.text.toString()))
+                type = "message/rfc822"
+                data = Uri.parse("mailto:")
+            }
+
+            val chooserIntent = Intent(ACTION_CHOOSER)
+            chooserIntent.putExtra(EXTRA_INTENT, sendMailIntent)
+            startActivity(chooserIntent)
+
+        }
     }
 }
